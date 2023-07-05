69d92d4c61c7a01476907a0470e3e3d86ec999f6
mfilippov <mfilippov@it-one.ru>
2023-07-05 16:29:41 +0300

    added jenkins files

---
+++ jenkins/pipelines/ui-tests.groovy
@@ -0,0 +1,52 @@
+timeout(180) {
+  node('gradle') {
+    timestamps {
+      wrap([$class: 'BuildUser']) {
+        summary = """|<b>Owner:</b> ${env.BUILD_USER}
+                            |<b>Branch:</b> ${BRANCH}""".stripMargin()
+        currentBuild.description = summary.replaceAll("\n", "<br>")
+        owner_user_id = "${env.BUILD_USER_ID}"
+      }
+      stage('Checkout') {
+        checkout scm
+      }K
+      stage('Run tests') {
+        tests_exit_code = sh(
+            script: "gradle test -Dbrowser=$BROWSER_NAME -DbrowserVersion=$BROWSER_VERSION -DwebDriverRemoteUrl=$GRID_URL",
+        )
+        if (tests_exit_code != 0) {
+          currentBuild.result = 'UNSTABLE'
+        }
+      }
+      stage('Publish artifacts') {
+        allure([
+            includeProperties: false,
+            jdk: '',
+            properties: [],
+            reportBuildPolicy: 'ALWAYS',
+            results: [[path: 'build/reports/allure-results']]
+        ])
+      }
+      stage('Publish notification in telegram') {
+        lines = readFile 'target/allure-results/export/influxDbData.txt'
+        def message = "============= UI REORT ============"
+        for (line in lines) {
+          def matcher = line =~ /.*(\w+=\d+).*/
+          message += line
+        }
+
+        def connection = null
+
+        connection = new URL(stringBuilder.toString()).openConnection() as HttpURLConnection
+        withCredentials([string(name: 'telegram_token', envVar: TELEGRAM_TOKEN)]) {
+          connection.setProperty("token", $TELEGRAM_TOKEN)
+          connection.setProperty("text", message)
+          connection.setProperty('chat_id': '-1111111')
+        }
+
+        connection.setRequestMethod('POST')
+        connection.setDoOutput(true)
+      }
+    }
+  }
+}
